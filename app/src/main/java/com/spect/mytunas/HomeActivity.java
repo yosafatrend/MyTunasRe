84cb20a29ee1e68f2406bf1d4f426ac73203b065
afifin <afifinihza@gmail.com>
2020-08-12 10:08:35 +0700

    wes gan

--- app/src/main/java/com/spect/mytunas/HomeActivity.java
+++ app/src/main/java/com/spect/mytunas/HomeActivity.java
@@ -1,14 +1,80 @@
 package com.spect.mytunas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
+    }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_news:
+                            openFragment(NewsFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_sekolah:
+                            openFragment(SchoolFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_job:
+                            openFragment(JobFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return true;
+
+                }
+            };
+
+    private void backStack(){
+        if(getSupportFragmentManager().getBackStackEntryCount()>1){
+            getSupportFragmentManager().popBackStack();
+        }else
+        if(getSupportFragmentManager().getBackStackEntryCount()==1){
+            this.finish();
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        boolean back = false;
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            back = true;
+            backStack();
+        }
+        return back;
+
+    }
+    public static void setHomeItem(Activity activity) {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                activity.findViewById(R.id.navigation_home);
+        bottomNavigationView.setSelectedItemId(R.id.home);
     }
 }
--- app/src/main/java/com/spect/mytunas/RegisterFragment.java
+++ app/src/main/java/com/spect/mytunas/RegisterFragment.java
@@ -1,13 +1,8 @@
 package com.spect.mytunas;
 
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
@@ -18,18 +13,17 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -78,6 +72,7 @@ public class RegisterFragment extends Fragment {
             Intent intent = new Intent(getActivity(), HomeActivity.class);
             Toast.makeText(getActivity(), "Anda sudah login", Toast.LENGTH_SHORT).show();
             startActivity(intent);
+            getActivity().finish();
         }
 
     }
