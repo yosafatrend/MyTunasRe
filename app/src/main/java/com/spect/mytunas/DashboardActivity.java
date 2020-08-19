21d932f83dd90697cb4e8cee735a1ce6a0a3561f
afifin <afifinihza@gmail.com>
2020-08-19 13:02:21 +0700

    penambahan home

--- app/src/main/java/com/spect/mytunas/DashboardActivity.java
+++ app/src/main/java/com/spect/mytunas/DashboardActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,6 +18,12 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("My Tunas");
+        }
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
--- app/src/main/java/com/spect/mytunas/HomeFragment.java
+++ app/src/main/java/com/spect/mytunas/HomeFragment.java
@@ -4,7 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
+
 import androidx.fragment.app.Fragment;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -38,6 +47,7 @@ public class HomeFragment extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +61,15 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        ImageSlider imageSlider=v.findViewById(R.id.slider);
+
+        List<SlideModel> slideModels=new ArrayList<>();
+        slideModels.add(new SlideModel("https://smkthpati.sch.id/images/berita/gb183.jpg"));
+        slideModels.add(new SlideModel("https://smkthpati.sch.id/images/galeri/gb184.jpg"));
+        slideModels.add(new SlideModel("https://smkthpati.sch.id/images/berita/gb178.jpg"));
+        slideModels.add(new SlideModel("https://lh3.googleusercontent.com/proxy/pDCifrkN6kbuoMQaW8rSIquKtLvRImOy3kLw_rz8VnAVIuQoKIBgsJDfEe1gNuXIrok_9CHl01lGpPPt_zhxydcwPXQM8XkpDcxjLgny3dzYcavIvSwDVt7b"));
+        imageSlider.setImageList(slideModels,true);
+        return v;
     }
 }
