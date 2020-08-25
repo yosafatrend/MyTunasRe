a95c64124f3998286e78909f13a172fc6f2bebf1
ARDINNO <visardinno@gmail.com>
2020-08-25 13:02:02 +0700

    menambah drawer layout

--- app/src/main/java/com/spect/mytunas/DashboardActivity.java
+++ app/src/main/java/com/spect/mytunas/DashboardActivity.java
@@ -2,26 +2,39 @@ package com.spect.mytunas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null){
             getSupportActionBar().setTitle("My Tunas");
+
+            drawerLayout = findViewById(R.id.drawerLayout);
+            NavigationView navigationView = (NavigationView) findViewById(R.id.drawer);
+            navigationView.setNavigationItemSelectedListener(this);
+            ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+            drawerLayout.addDrawerListener(drawerToggle);
+            drawerToggle.syncState();
         }
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -41,5 +54,34 @@ public class DashboardActivity extends AppCompatActivity {
         a.addCategory(Intent.CATEGORY_HOME);
         a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(a);
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            closeDrawer();
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem Item) {
+        String itemName = (String) Item.getTitle();
+
+        closeDrawer();
+
+        switch (Item.getItemId()){
+            case R.id.nav_setting:
+                break;
+            case R.id.nav_Logout:
+                break;
+        }
+        return false;
+    }
+
+    private void closeDrawer() {
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    private void openDrawer(){
+        drawerLayout.openDrawer(GravityCompat.START);
+
     }
 }
