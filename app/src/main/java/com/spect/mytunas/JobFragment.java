1c7f0784744200c5931da4ad81457a34c1d93246
afifin <afifinihza@gmail.com>
2020-08-31 13:40:05 +0700

    penambahan pada fragment job

--- app/src/main/java/com/spect/mytunas/JobFragment.java
+++ app/src/main/java/com/spect/mytunas/JobFragment.java
@@ -4,59 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link JobFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class JobFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM2 = "param2";
-    private static final String ARG_PARAM1 = "param1";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public JobFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static JobFragment newInstance(String param1, String param2) {
-        JobFragment fragment = new JobFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job, container, false);
+        View v = inflater.inflate(R.layout.fragment_job, container, false);
+        final AutoCompleteTextView actv1 = (AutoCompleteTextView) v.findViewById(R.id.actv1);
+        ImageView image = (ImageView) v.findViewById(R.id.image);
+        actv1.setThreshold(2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_dropdown_item_1line,items);
+        actv1.setAdapter(adapter);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actv1.showDropDown();
+            }
+        });
+        return v;
     }
+    private static final String[] items = new String[]{"Pati, Jateng","Rembang, Jateng", "Kudus, Jateng"};
 }
